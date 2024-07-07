0f7d82fc837cf404abc2cc4af2005747e6db6d65
hezekv <hezekiahvaldez@gmail.com>
2024-07-08 02:55:40 +0800

    updated javascript 4

--- js/plugins.js
+++ js/plugins.js
@@ -4246,7 +4246,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 1,
+        "speed": 3,
         "direction": "none",
         "random": false,
         "straight": false,
